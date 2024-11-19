701606d5eceafe1be912a94f82fde23496dae717
dmitsmirnov <dmitsmirnov@bk.ru>
2024-11-19 17:53:53 +0300

    update

--- BaseProject/ImageOnMainUIView.swift
+++ BaseProject/ImageOnMainUIView.swift
@@ -14,6 +14,7 @@ struct ImageOnMainUIView: View {
     @State private var imageOpacity: Double = 1.0 // Прозрачность изображения
     
     var body: some View {
+        
         ZStack(alignment: .top) {
             // Фоновое изображение с изменяемой прозрачностью
             Image("USA") // Замените на имя вашего изображения
@@ -39,25 +40,30 @@ struct ImageOnMainUIView: View {
                             
                             imageOpacity = offset / 359
                             
-                            print(offset)
+                            //print(offset)
                             
                         }
                 }
                 .frame(height: 0) // Высота 0, чтобы не занимать место
                 
+                //Form {
                 VStack(spacing: 16) {
                     // Пара заголовков или контента
-                    ForEach(0..<20, id: \.self) { index in
-                        Text("Контент \(index + 1)")
-                            .font(.title)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(8)
-                            .shadow(radius: 4)
+                    Section{
+                        ForEach(0..<20, id: \.self) { index in
+                            Text("Контент \(index + 1)")
+                                .font(.title)
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(8)
+                                .shadow(radius: 4)
+                        }
                     }
                 }
-                .padding(.top, 250) // Отступ сверху
+                //.padding(.top, 150) // Отступ сверху
+                .offset(y: 200)
+                //.contentMargins(150)
             }
         }
     }
